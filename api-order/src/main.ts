1aa80104496078d0da6092a58472d581597aa7a0
Leonardo Anjos <leodrums11@gmail.com>
2023-04-15 13:42:58 -0300

    feat: filters - a middleware to catch errors

--- api-order/src/main.ts
+++ api-order/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionFilter } from './infrastructure/exceptions/exceptions.filter';
+import { LoggerService } from './infrastructure/logger/logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new AllExceptionFilter(new LoggerService()));
   await app.listen(3000);
 }
 bootstrap();
