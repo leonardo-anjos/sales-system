e8f58fccd5427e612535c8e8e37a0ce9ac951010
Leonardo Anjos <leodrums11@gmail.com>
2023-04-15 02:46:01 -0300

    feat: logger

--- api-order/src/app.module.ts
+++ api-order/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
 import { TypeormModule } from './infrastructure/config/typeorm/typeorm.module';
+import { LoggerModule } from './infrastructure/logger/logger.module';
 
 @Module({
-  imports: [EnvironmentConfigModule, TypeormModule],
+  imports: [EnvironmentConfigModule, TypeormModule, LoggerModule],
 })
 export class AppModule {}
