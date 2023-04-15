c91c14a28d060eff24e1241992da44dc2cba86ae
Leonardo Anjos <leodrums11@gmail.com>
2023-04-15 13:49:37 -0300

    feat: working with  interceptors

--- api-order/src/main.ts
+++ api-order/src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './infrastructure/exceptions/exceptions.filter';
 import { LoggerService } from './infrastructure/logger/logger.service';
+import { LoggingInterceptor } from './infrastructure/logger/logger.interceptor';
+import { ResponseInterceptor } from './infrastructure/logger/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionFilter(new LoggerService()));
+  app.useGlobalInterceptors(new LoggingInterceptor(new LoggerService()));
+  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(3000);
 }
 bootstrap();
