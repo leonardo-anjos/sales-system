31300ab5d237218f4c000597d2517a3719d04d69
Leonardo Anjos <leodrums11@gmail.com>
2023-04-15 02:50:06 -0300

    feat: exceptions

--- api-order/src/app.module.ts
+++ api-order/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
 import { TypeormModule } from './infrastructure/config/typeorm/typeorm.module';
 import { LoggerModule } from './infrastructure/logger/logger.module';
+import { ExceptionsModule } from './infrastructure/exceptions/exceptions.module';
 
 @Module({
-  imports: [EnvironmentConfigModule, TypeormModule, LoggerModule],
+  imports: [EnvironmentConfigModule, TypeormModule, LoggerModule, ExceptionsModule],
 })
 export class AppModule {}
