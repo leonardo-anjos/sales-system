6f97a223de7a0873313573c88b3b67a84c292fa0
Leonardo Anjos <leodrums11@gmail.com>
2023-04-15 16:08:01 -0300

    feat: importing modules and swagger config

--- api-order/src/app.module.ts
+++ api-order/src/app.module.ts
@@ -1,27 +1,17 @@
-import { Module } from '@nestjs/common';
-import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
-import { TypeormModule } from './infrastructure/config/typeorm/typeorm.module';
-import { LoggerModule } from './infrastructure/logger/logger.module';
-import { ExceptionsModule } from './infrastructure/exceptions/exceptions.module';
-import { RepositoriesModule } from './domain/repositories/repositories.module';
-import { UsecasesProxyModule } from './usecases/usecases-proxy/usecases-proxy.module';
-import { GetOrdersUsecasesService } from './usecases/get-orders-usecases/get-orders-usecases.service';
-import { ControllersController } from './infrastructure/controllers/controllers.controller';
-import { ControllersModule } from './infrastructure/controllers/controllers.module';
-import { RepositoriesModule } from './infrastructure/repositories/repositories.module';
-import { UsecasesProxyModule } from './infrastructure/usecases-proxy/usecases-proxy.module';
+import { Module } from "@nestjs/common";
+import { EnvironmentConfigModule } from "./infrastructure/config/environment-config/environment-config.module";
+import { ControllersModule } from "./infrastructure/controllers/controllers.module";
+import { ExceptionsModule } from "./infrastructure/exceptions/exceptions.module";
+import { LoggerModule } from "./infrastructure/logger/logger.module";
+import { UsecasesProxyModule } from "./infrastructure/usecases-proxy/usecases-proxy.module";
 
 @Module({
   imports: [
-    EnvironmentConfigModule,
-    TypeormModule,
     LoggerModule,
     ExceptionsModule,
-    RepositoriesModule,
-    UsecasesProxyModule,
+    UsecasesProxyModule.register(),
     ControllersModule,
+    EnvironmentConfigModule,
   ],
-  providers: [GetOrdersUsecasesService],
-  controllers: [ControllersController],
 })
 export class AppModule {}
